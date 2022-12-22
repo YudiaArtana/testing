47304f338619bdadd8d7f720b4f1704aded456f4
Yudia Artana <yudiaartana87@gmail.com>
2022-12-22 22:35:24 +0800

    testing1

---
+++ index.js
@@ -0,0 +1,12 @@
+function nama(){
+  namalengkap = "Yudia Artana"
+  return namalengkap;
+}
+
+function namalengkaplagi(tes){
+  nana = nama();
+  a = nana + tes
+  return a;
+}
+
+console.log(nama(), "dan", namalengkaplagi("hahah"))
